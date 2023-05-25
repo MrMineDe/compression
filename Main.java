49937475bb73e3ddcef22f1db281da13c9fc961f
mrmine <mrmine1@proton.me>
2023-05-26 00:46:13 +0200

    initial lzw encode - not finished

--- Main.java
+++ Main.java
@@ -2,6 +2,34 @@ import java.io.*;
 
 public class Main {
 
+	public static String lzw_encode(String input){
+		DynArray<String> dictionary = new DynArray<String>();
+		String output = new String(""),
+		       previous_word = new String(""),
+		       current_word = new String("");
+		boolean dublicate = false;
+
+		for(int i=0; i < input.length(); i++){
+			current_word = String.valueOf(input.charAt(i));
+			for(int j=0; j < dictionary.getLength(); j++){
+				if((previous_word+current_word) == dictionary.getItem(i)){
+					dublicate = true;
+					break;
+				}
+			}
+			if(!dublicate){
+				dictionary.append((previous_word)+current_word);
+				previous_word = "" + current_word;
+				output = (output + previous_word);
+			} else {
+				previous_word = (previous_word + current_word);
+			}
+		}
+
+
+		return output;
+	}
+
 	public static DynArray<Boolean> appendToBooleanArray(DynArray<Boolean> orig, Boolean[] appends){
 		for(int i=0; i < appends.length; i++){
 			orig.append(appends[i]);
